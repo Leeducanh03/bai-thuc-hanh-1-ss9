c980296d9e78c5435a543bb2e6897287198bda7f
leeducanh03 <leeducanh03@gmail.com>
2023-07-20 13:40:06 +0700

    ss9

---
+++ main.js
@@ -0,0 +1,11 @@
+let year = +prompt("Nhập vào số năm ");
+if (year % 4 == 0)
+    if (year % 100 == 0) {
+        if (year % 400 != 0) {
+            isLeafYear = false;
+        } else {
+            // nhuận
+        }
+    } else {
+        // không nhuận
+    }
